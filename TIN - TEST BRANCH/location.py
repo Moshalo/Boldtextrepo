1a09fd889406da1737ce048c855bc08f7867bb63
Moshalo <38192441+Moshalo@users.noreply.github.com>
2018-08-06 12:08:39 -0400

    8/6/18 Update #2
    Continued work on worldspace system, working on persistent locations, and non-persistent camps that you can wipe out and get a chest of loot at the end. This will continue work into the dungeon system later on.

--- TIN - TEST BRANCH/location.py
+++ TIN - TEST BRANCH/location.py
@@ -1,3 +1,6 @@
+from enemyDB import enemyDB as EDB
+
+
 class town():
     def __init__(self, name, pos, bank, bankamount, blacksmith, gunsmith, generalstore):
         self.name = name
@@ -8,18 +11,31 @@ class town():
         self.gunsmith = gunsmith
         self.generalstore = generalstore
 
+class camp():
+    def __init__(self, name, pos, enemies, endloot, description):
+        self.name = name
+        self.pos = pos
+        self.enemies = enemies
+        self.endloot = endloot
+        self.description = description
+
+class campDB():
+    bcamp1 = camp("Bandit Camp", 42, [], [], "A small campsite with bandits around the campfire..")
+
 class townDB():
     ttown = town("Test Town", 12, True, 60000, [], [], [])
 
 class dynl():
-    def __init__(self, pos, town, locations, npcs):
+    def __init__(self, pos, town, locations, npcs, enemies, description):
         self.pos = pos
         self.town = town
         self.locations = locations
         self.npcs = npcs
+        self.enemies = enemies
+        self.description = description
 
 class dynlDB:
-    p12 = dynl(12, townDB.ttown, [], [])
-
+    p12 = dynl(12, townDB.ttown, [], [], [], "The outskirts of Test Town..")
+    p42 = dynl(42, None, [], [], [EDB.fban, EDB.fban, EDB.fban], "A bandit camp lies ahead... type 'attack camp' if you want to fight")
     pointsofinterest = [p12]
 
